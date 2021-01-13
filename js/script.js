2e208b5e9a0e57c3c0ccfcec431ba85d52d1ba6c
Tanuj Dey <tanujdey7@gmail.com>
2021-01-14 01:06:30 +0530

    backend

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-const buttons = document.querySelectorAll('a');
-buttons.forEach(btn => {
-    btn.addEventListener('click', function(e) {
-      let x = e.clientX = e.target.offsetLeft;  
-      let y = e.clientY = e.target.offsetTop;  
-      let ripples = document.createElement('span');
-      ripples.style.left = x + 'px';
-      ripples.style.top = y + 'px';
-      this.appendChild(ripples);
-      setTimeout(() =>  {
-          ripples.remove()
-      },1000);
-    })
-})
+function payment() {
+  var amt = document.getElementById('amount').value*100;
+  var options = {
+    "key": "rzp_test_sTtj1ICq12Op7e",
+    "amount": amt, // Example: 2000 paise = INR 20
+    "name": "Tanuj Dey",
+  "handler": function (response) {
+    console.log(response);
+      // AFTER TRANSACTION IS COMPLETE YOU WILL GET THE RESPONSE HERE.
+    },
+  };
+  console.log(options);
+  var propay = new Razorpay(options);
+propay.open();
+}
