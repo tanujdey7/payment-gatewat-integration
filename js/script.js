9be9b5dac33d267f8cda7ffdc8075b06cc7e7159
Tanuj Dey <tanujdey7@gmail.com>
2021-01-14 22:25:53 +0530

    code optimized

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function payment() {
   var amt = document.getElementById('amount').value*100;
   var options = {
-    "key": "rzp_test_sTtj1ICq12Op7e",
+    "key": "rzp_test_L5aaES5fAUkb2I",
     "amount": amt, // Example: 2000 paise = INR 20
     "name": "Tanuj Dey",
   "handler": function (response) {
